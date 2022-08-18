30632a6f90372fffd04516dbb142490273578cd5
Jason Howmans <jason@nosaj.io>
2022-08-18 01:23:00 +0100

    Add logos section and style for small screens

--- src/components/homeLogos/homeLogos.tsx
+++ src/components/homeLogos/homeLogos.tsx
@@ -1,4 +1,24 @@
+import clsx from 'clsx';
+import s from './homeLogos.module.css';
+
+const logos = ['frontier', 'pave', 'bbc', 'firstminute'];
 
 export const HomeLogos = () => {
-  return <div>logos</div>;
+  return (
+    <div className="container pt">
+      <div className="content">
+        <section className={s.home_logos}>
+          <h2 className={s.logos_title}></h2>
+          <ul className={s.logos_list}>
+            {logos.map((l, i) => (
+              <li
+                key={i}
+                className={clsx(s.logos_list_item, s[`${l}-logo`])}
+              ></li>
+            ))}
+          </ul>
+        </section>
+      </div>
+    </div>
+  );
 };
