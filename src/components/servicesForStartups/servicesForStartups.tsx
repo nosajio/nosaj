64344955d084e6f2b40f8c3f3022d5b18fea35de
Jason Howmans <jason@nosaj.io>
2022-08-20 14:58:36 +0100

    Add SemiBold Silka font

--- src/components/servicesForStartups/servicesForStartups.tsx
+++ src/components/servicesForStartups/servicesForStartups.tsx
@@ -4,8 +4,8 @@ import { useBreakpoint } from '../../hooks';
 import s from './servicesForStartups.module.css';
 
 const services = [
-  'Prototyping.',
-  'Product strategy / finding product market fit.',
+  'Product strategy.',
+  'Finding product market fit.',
   'Product & UI design.',
   'Engineering.',
 ];
@@ -35,21 +35,23 @@ export const ServicesForStartups = () => {
               customer development, marketing, and startup building.
             </p>
             <p>
-              With enough repetition I&apos;ve learned how to ship products at
-              different scales that meet specific goals, deliver returns, and
-              please users.
+              Through repetition I&apos;ve learned how to ship products at
+              different scales, that{' '}
+              <strong>
+                meet specific goals, deliver returns, and please users
+              </strong>
+              .
             </p>
             <p>
-              I offer an advantage to startups by coming in with a broad set of
-              skills and experiences usually filled by multiple hires.
+              I offer a unique advantage to startups by coming in with a broad
+              set of skills and experiences often found by hiring multiple
+              people.
             </p>
           </div>
           <div className={s.services_visual}></div>
           <div className={s.services_list}>
             <IconList circleIcon compact icon="check" smallText={isLtMedium}>
-              {services.map(service => (
-                <>{service}</>
-              ))}
+              {services}
             </IconList>
           </div>
           <footer className={s.services_footer}>
