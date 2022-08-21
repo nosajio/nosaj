6ae54c53efd3605b3b745afb024fe6e6d846733a
Jason Howmans <jason@nosaj.io>
2022-08-22 00:22:01 +0100

    Deploy scripts

---
+++ ecosystem.config.js
@@ -0,0 +1,34 @@
+const SSH_USER = process.env.SSH_USER;
+const SSH_HOST = process.env.SSH_HOST;
+
+if (!SSH_USER || !SSH_HOST) {
+  console.error('Missing env vars');
+  process.exit(1);
+}
+
+module.exports = {
+  apps: [
+    {
+      script: 'npm start',
+      watch: '.',
+      name: 'nosaj.io',
+      instances: 1,
+      max_restarts: 20,
+      env: {
+        PORT: 5420,
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: SSH_USER,
+      host: SSH_HOST,
+      ref: 'origin/master',
+      repo: 'https://github.com/nosajio/nosaj',
+      path: '/home/apps/nosajio_production',
+      'post-deploy':
+        'npm install && pm2 reload ecosystem.config.js --env production',
+    },
+  },
+};
