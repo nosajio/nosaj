e161c16002cd757ad3ab263d6f6904ca04eb83b9
Jason Howmans <jason@nosaj.io>
2022-08-22 01:31:39 +0100

    Add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,33 @@
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
+
 import 'normalize.css';
 import '../styles/vars.css';
 import '../styles/fonts.css';
-import '../styles/layout.css';
 import '../styles/globals.css';
+import '../styles/layout.css';
 
 function Nosajio({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Google analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=UA-122370645-1"
+        strategy="afterInteractive"
+      />
+      <Script strategy="afterInteractive" id="ga">
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'UA-122370645-1');
+          `}
+      </Script>
+
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default Nosajio;
